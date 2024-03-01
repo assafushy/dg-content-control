9110b0625edb54798d68c74f8bc6237fe8ed4662
amirlandau <amirlandau5@gmail.com>
2024-03-01 17:51:32 +0200

    s

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -287,7 +287,8 @@ export default class DgContentControls {
         if (skin.type === 'paragraph' && skin.runs.some(run => run.text === 'Test Description:')) {
             return; // Skip this skin
         }
-            contentControl.wordObjects.push(skin);
+        console.log("skin", skin)
+        contentControl.wordObjects.push(skin);
     });
       return contentControl;
     } catch (error) {
--- src/factories/RichTextDataFactory.ts
+++ src/factories/RichTextDataFactory.ts
@@ -79,6 +79,7 @@ export default class RichTextDataFactory {
   };
 
   async htmlStrip() {
+    console.log(this.richTextString, "this.richTextString")
     this.stripedString = striphtml(this.richTextString, {
       cb: this.replaceTags,
     }).result;
@@ -219,6 +220,7 @@ export default class RichTextDataFactory {
   }
 
   paragraphDataParser(contentString: string) {
+    console.log("contentString", contentString)
     let paragraphData = { fields: [{ name: "text", value: contentString }] };
     this.skinDataContentControls.push({
       type: "paragraph",
