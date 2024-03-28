0964a71264614a8962543e3afcb8987d6e6a453c
amirlandau <amirlandau5@gmail.com>
2024-03-28 10:48:53 +0200

    remove empty lines

--- src/factories/TestDataFactory.ts
+++ src/factories/TestDataFactory.ts
@@ -312,13 +312,11 @@ export default class TestDataFactory {
                           // Skip this iteration and move to the next one
                           return null;
                         }
-                        
                         let action = richTextFactoryAction.skinDataContentControls[0].data.fields[0].value;
                         let expected = richTextFactoryExpected.skinDataContentControls[0].data.fields[0].value;
-
+                        
                         action = action.replace(/\n/g, "<BR/>");
                         expected = expected.replace(/\n/g, "<BR/>");
-
 
                         let testStepAttachments = testCase.attachmentsData.filter(
                           attachment => {
